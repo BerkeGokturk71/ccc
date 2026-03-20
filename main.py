d521b51964879a080237588b421ea4780e1e6878
Berkehan Gokturk <berkehan7119@gmail.com>
2026-03-20 05:56:12 +0300

    a

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ from flask import app
 from socket_engine import start_socket_engine
 from web_app import run_flask
 
-if __name__ == '__main__':
-    # Önce botu başlatıyoruz ama thread olarak
-    print("🤖 Bot thread'i hazırlanıyor...")
-    t = threading.Thread(target=start_socket_engine, daemon=True)
-    t.start()
-    
-    # Sonra Flask'ı Render'ın istediği portta açıyoruz
-    port = int(os.environ.get("PORT", 10000))
-    print(f"🌐 Flask {port} portunda açılıyor...")
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+   def run_flask():
+    # Koyeb'in verdiği portu al, yoksa 8000 kullan
+    port = int(os.environ.get("PORT", 8000)) 
+    app.run(host='0.0.0.0', port=port)
